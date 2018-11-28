b7a1d959928b210ef17b6e61f759e92bd408d847
Emmanuel <emmanuel.utrum@pm.me>
2018-11-28 14:37:29 +0000

    Update coins.js

--- src/coins.js
+++ src/coins.js
@@ -17,69 +17,18 @@ const coins = [
     ],
   },
   {
-    ticker: 'TESTBTC',
-    name: 'bitcoin',
-    network: bitcoinjs.networks.testnet,
-    path: "m/44'/0'/0'",
-    explorer: 'https://test-insight.bitpay.com',
-    electrum: [
-      {
-        host: 'test-btc.electrum.monaize.com', port: 10000, mode: 'tcp',
-      },
-    ],
-  },
-  {
     ticker: 'KMD',
     name: 'komodo',
     network: bitcoinjs.networks.komodo,
     fee: 10000,
     path: "m/44'/141'/0'",
-    explorer: 'https://kmdexplorer.ru',
+    explorer: 'https://kmdexplorer.io',
     electrum: [
       {
         host: 'electrum1.cipig.net', port: 10001, mode: 'tcp',
       },
       {
         host: 'electrum2.cipig.net', port: 10001, mode: 'tcp',
-      },
-    ],
-  },
-  {
-    ticker: 'TESTKMD',
-    name: 'komodo',
-    network: bitcoinjs.networks.komodo,
-    path: "m/44'/141'/0'",
-    fee: 10000,
-    explorer: 'https://kmd.explorer.monaize.com',
-    electrum: [
-      {
-        host: 'test-ac.electrum.monaize.com', port: 10001, mode: 'tcp',
-      },
-    ],
-  },
-  {
-    ticker: 'MNZ',
-    name: 'monaize',
-    network: bitcoinjs.networks.monaize,
-    path: "m/44'/141'/0'",
-    fee: 0,
-    explorer: 'https://www.mnzexplorer.com/',
-    electrum: [
-      {
-        host: 'mnz.electrum.monaize.com', port: 10002, mode: 'tcp',
-      },
-    ],
-  },
-  {
-    ticker: 'TESTMNZ',
-    name: 'monaize',
-    network: bitcoinjs.networks.monaize,
-    path: "m/44'/141'/0'",
-    fee: 0,
-    explorer: 'https://mnz.explorer.monaize.com',
-    electrum: [
-      {
-        host: 'test-ac.electrum.monaize.com', port: 10002, mode: 'tcp',
       },
     ],
   },
