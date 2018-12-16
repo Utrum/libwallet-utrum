350ddfef43a99b8c19cd2c502f2724f05e7a68a0
Undefined <emmanuel.utrum@pm.me>
2018-12-16 04:10:56 +0000

    new network definitions for sapling support

--- src/networks.js
+++ src/networks.js
@@ -1,4 +1,4 @@
-const komodo = {
+/* const komodo = {
   messagePrefix: '\x18Komodo Signed Message:\n',
   bip32: {
     public: 0x0488B21E,
@@ -7,6 +7,29 @@ const komodo = {
   pubKeyHash: 0x3c,
   scriptHash: 0x55,
   wif: 0xbc,
+} */
+
+const komodo = {
+    messagePrefix: '\x19Komodo Signed Message:\n',
+    bip44: 141,
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4,
+    },
+    pubKeyHash: 0x3c,
+    scriptHash: 0x55,
+    wif: 0xbc,
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb,
+    },
+    dustThreshold: 1000,
+    isZcash: true,
+    sapling: true,
+    saplingActivationTimestamp: 1544835600,
+    kmdInterest: true,
 }
 
 const monaize = Object.assign({}, komodo);
