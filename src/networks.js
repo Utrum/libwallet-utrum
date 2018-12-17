799bd45d184dc9f2ee1ac2f8f5d73d18138124a4
Undefined <emmanuel.utrum@pm.me>
2018-12-17 02:31:47 +0000

    added komodo network

--- src/networks.js
+++ src/networks.js
@@ -9,40 +9,30 @@ const komodo_nonsapling = {
   wif: 0xbc,
 }
 
-/*
 const komodo = {
-    messagePrefix: '\x19Komodo Signed Message:\n',
-    bip44: 141,
-    bech32: 'bc',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4,
-    },
-    pubKeyHash: 0x3c,
-    scriptHash: 0x55,
-    wif: 0xbc,
-    consensusBranchId: {
-      1: 0x00,
-      2: 0x00,
-      3: 0x5ba81b19,
-      4: 0x76b809bb,
-    },
-    dustThreshold: 1000,
-    isZcash: true,
-    sapling: true,
-    saplingActivationTimestamp: 1544835600,
-    kmdInterest: true,
-    coin: 'komodo'
-} */
-
-/* const monaize = Object.assign({}, komodo);
-monaize.messagePrefix = '\x18Monaize Signed Message:\n'; */
+  messagePrefix: '\x19Komodo Signed Message:\n',
+  bip44: 141,
+  bech32: 'bc',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4,
+  },
+  pubKeyHash: 0x3c,
+  scriptHash: 0x55,
+  wif: 0xbc,
+  consensusBranchId: {
+    1: 0x00,
+    2: 0x00,
+    3: 0x5ba81b19,
+    4: 0x76b809bb,
+  },
+  coin: 'zec'
+}
 
 const utrum = Object.assign({}, komodo_nonsapling);
 utrum.messagePrefix = '\x18Utrum Signed Message:\n';
 
 module.exports = {
-  //komodo,
-  //monaize,
+  komodo,
   utrum
 }
