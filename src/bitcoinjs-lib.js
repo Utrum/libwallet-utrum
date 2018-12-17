bf28e3c0b92338bc26746258fbfe5067204293a2
Undefined <emmanuel.utrum@pm.me>
2018-12-17 02:57:14 +0000

    uncomments important line

--- src/bitcoinjs-lib.js
+++ src/bitcoinjs-lib.js
@@ -1,8 +1,7 @@
 const bitcoinjs = require('bitgo-utxo-lib');
 const networks = require('./networks');
 
-//bitcoinjs.networks.komodo = networks.komodo;
-//bitcoinjs.networks.monaize = networks.monaize;
+bitcoinjs.networks.komodo = networks.komodo;
 bitcoinjs.networks.utrum = networks.utrum;
 
 module.exports = bitcoinjs;
